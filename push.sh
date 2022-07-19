6022555762f526cb28a0091bfae9a9347e9c2531
RafaMtF <rapido44845@hotmail.com>
2022-07-19 11:37:04 -0300

    Revision:

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 cd /home/rafa/Git
 git add --all
 git commit -m "Revision: $(crc32 <(echo $(date)))"
-git branch -M master >/dev/null 2>&1
+git branch -M main >/dev/null 2>&1
 git remote add origin git@github.com:/RafaMtF/dotfiles.git > /dev/null 2>&1
-git push -u origin master > /dev/null 2>&1
+git push -u origin main > /dev/null 2>&1
 git push
